4e098390d5eaec78e2d64d3ce7620a58741b6a10
Gabe Le <legabriel176@yahoo.com>
2018-05-19 17:28:27 -0400

    Added Division function

--- calculator.py
+++ calculator.py
@@ -2,10 +2,11 @@ def add(num1, num2):
 	return num1+num2
 
 def subtract(num1, num2):
-    return num1 + num2
+	return num1-num2
 
-def printf(format, *values):
-    print(format % values )
+def division(num1, num2):
+	return num1/num2
+
 
 def checkInput(num):
 	while(True):
@@ -22,15 +23,20 @@ def main():
 	num2 = checkInput(num)
 	
 	while(True):
-		print('Enter a choice')
+		print('\nEnter a choice')
 		choice = checkInput(num)
 		if(choice == 1):
 			print('Addition')
 			print(add(num1, num2))
-        elif(choice == 2):
-            print(subtract(num1, num2))
+		elif(choice == 2):
+			print('Subtraction')
+			print(subtract(num1, num2))
+		elif(choice == 3):
+			print('Division')
+			print(division(num1,num2))
 		else:
-			print('Please enter a valid choice')
+			print('Goodbye')
+			exit(0)
 
 
 main()
