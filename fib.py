49f2943dab93819a13e911aaa15453019c525a07
Gabe Le <legabriel176@yahoo.com>
2018-05-19 13:13:47 -0400

    added the factorial function

--- fib.py
+++ fib.py
@@ -4,9 +4,17 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-
+def factorial(n):
+    if(n <= 1):
+        return 1
+    else:
+        return n * factorial(n-1)
 
 def main():
     num = 5
     print("I am going to calculate the fibonacci series")
     print(fib(num))
+    print("Now I will calculate the factorial series")
+    print(factorial(num))
+
+main()
